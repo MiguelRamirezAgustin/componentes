6d90d85d234819ed523b7dd0be19d7c6e3e9aeaa
MiguelRamirezAgustin <miguel.r.agustin@gmail.com>
2018-12-19 10:33:27 -0600

    login en optionDialogo Diseño

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -11,7 +11,7 @@ var dateupdate=false;
 var calendario=Ti.UI.createImageView({
 	top:"10%",
 	width:"13%",
-	height:"8%",
+	height:"12%",
 	image:"/images/calendar.png"
 });
 $.viewCalendario.add(calendario);
@@ -28,13 +28,13 @@ $.viewCalendario.add(dateValue);
 
 var texfiel=Ti.UI.createTextField({
 	width:"50%",
-	height:"8%",
+	height:"15%",
 	color:"red",
 	top:"15%",
 	borderColor:"blue",
 	borderRadius:5,
 	font:{
-		fontSize:15
+		fontSize:12
 	}
 });
 $.viewCalendario.add(texfiel);
@@ -57,6 +57,51 @@ calendario.addEventListener('click', function(){
 		}
 	});
 });
+
+var loginView=Ti.UI.createView({
+	layout:"vertical",
+	width:"90%",
+	height:"70%"
+});
+
+var txtUserName =Ti.UI.createTextField({
+	hintText:'User Name',
+	hintTextColor:"gray",
+	top:"4%",
+	width:Ti.UI.FILL,
+	height:"10%",
+	borderColor:"gray",
+	color:"white",
+	borderRadius:3,
+	textAlign:Ti.UI.TEXT_ALIGNMENT_CENTER
+});
+
+var txtUserPassword =Ti.UI.createTextField({
+	hintText:'password',
+	hintTextColor:"gray",
+	top:"4%",
+	width:Ti.UI.FILL ,
+	height:"10%",
+	borderColor:"gray",
+	color:"white",
+	borderRadius:3,
+	textAlign:Ti.UI.TEXT_ALIGNMENT_CENTER
+});
+
+loginView.add(txtUserName);
+loginView.add(txtUserPassword);
+
+
+$.lbdialogo.addEventListener('dblclick', function(){
+	var optiondialogo=Ti.UI.createOptionDialog({
+	 title:"Login",
+	 androidView:loginView,
+	 buttonNames:['login','cancel']
+	});
+	optiondialogo.show();
+});
+
+
 
 $.index.open();
 
